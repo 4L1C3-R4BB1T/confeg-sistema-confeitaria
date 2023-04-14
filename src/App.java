7fbb65a49ff206d678ee1b04d5abd348722720aa
Gabriel Cardoso <gabrielcardosogirarde@gmail.com>
2023-04-13 23:29:59 -0300

    Revert "Tudo deletado"
    This reverts commit a20cfc88902da237b4d22ef6330b98dccb8f43e9.

---
+++ src/App.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@FunctionalInterface
+interface Conversor<T, R> {
+    R converter(T t);
+}
+
+class Pessoa implements Comparable<Pessoa> {
+    String nome;
+    Integer idade;
+
+    Pessoa(String nome, Integer idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+    
+    @Override 
+    public int compareTo(Pessoa other) {
+       if (this.idade > other.idade) {
+            return 1;
+       } else if (this.idade < other.idade) {
+            return -1;
+       }
+       return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "nome: " + nome + " - idade: " + idade;
+    }
+}
+
+
+public class App {
+
+    public static void main(String[] args) {
+        
+        Conversor<String, Double> lambda = new Conversor<String, Double>() {
+            @Override 
+            public Double converter(String v) {
+                final String regex = "^\\d+(\\.\\d+)*$";
+                if (v != null && v.matches(regex)) {
+                    return Double.parseDouble(v);
+                } else {
+                    return null;
+                }
+            }
+        };
+
+        System.out.println(lambda.converter("125.24"));
+
+
+        List<Pessoa> pessoas = new ArrayList<>();
+
+        pessoas.add(new Pessoa("Livia", 22));
+        pessoas.add(new Pessoa("Gabriel", 23));
+        pessoas.add(new Pessoa("Marcos", 15));
+
+        Collections.sort(pessoas);
+        pessoas.forEach(System.out::println);
+    }
+}
+
