628346ccfb4867eecb9dc4a9fca714a74a002dbc
Gabriel Cardoso <gabrielcardosogirarde@gmail.com>
2023-04-13 22:56:15 -0300

    Adição da função funcional para converter um numero double

--- src/App.java
+++ src/App.java
@@ -1,7 +1,28 @@
+
+@FunctionalInterface
+interface Conversor<T, R> {
+    R converter(T t);
+}
+
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+        
+        Conversor<String, Double> lambda = new Conversor<String, Double>() {
+            @Override 
+            public Double converter(String v) {
+                final String regex = "^\\d+(\\.\\d+)*$";
+                if (v != null && v.matches(regex)) {
+                    return Double.parseDouble(v);
+                } else {
+                    return null;
+                }
+            }
+        };
 
+        System.out.println(lambda.converter("125.24"));
+
+
+    }
 }
