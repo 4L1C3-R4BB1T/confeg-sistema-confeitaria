01884bf31da52a999949e03abc4815c7b814e587
Gabriel Cardoso <gabrielcardosogirarde@gmail.com>
2023-04-14 16:08:06 -0300

    Efeito de mudança de backgroud na tela login

--- src/controladores/LoginControlador.java
+++ src/controladores/LoginControlador.java
@@ -1,5 +1,6 @@
 package controladores;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,6 +9,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import modelos.dao.DAO;
@@ -21,7 +23,21 @@ public class LoginControlador {
     @FXML
     private TextField campoSenha;
 
+    @FXML
+    private StackPane areaDeFoto;
+
+    private String[] fotos = {
+        getClass().getResource("/telas/login/images/background-1.png").toExternalForm(),
+        getClass().getResource("/telas/login/images/background-2.png").toExternalForm(),
+        getClass().getResource("/telas/login/images/background-3.png").toExternalForm()
+    };
+
+    private int fotoAtual = 0;
+
+    
+
     private Stage palcoLogin;
+
 
     @FXML
     public void entrar(ActionEvent event) throws Exception {
@@ -70,5 +86,20 @@ public class LoginControlador {
     public void setPalco(Stage palcoLogin) {
         this.palcoLogin = palcoLogin;
     } 
+
+    @FXML
+    public void initialize() {
+        new Thread(() -> {
+            while (true) {
+                try {
+                    Platform.runLater(() -> {
+                        areaDeFoto.setStyle(String.format("-fx-background-image: url('%s') !important;", fotos[fotoAtual++ % fotos.length]));
+                    });
+                    Thread.sleep(3000);
+
+                } catch (Exception erro) {}
+            }
+        }).start();
+    }
 
 }
