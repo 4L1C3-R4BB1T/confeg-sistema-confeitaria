d18c3fafd8b128a954a126320439e094fb812524
Livia <livinha.guimaraes.013@gmail.com>
2023-04-13 22:45:03 -0300

    adicionado for que conta de 0 até 10

--- src/App.java
+++ src/App.java
@@ -2,6 +2,12 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("num: " + i);
+        }
     }
 
 }
