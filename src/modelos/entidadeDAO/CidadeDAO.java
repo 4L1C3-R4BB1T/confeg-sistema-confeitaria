aee57b6ce289824d53adedff016085e360cb2af5
Livia <livinha.guimaraes.013@gmail.com>
2023-04-15 11:01:52 -0300

    resolvendo conflito na classe CidadeDAO

--- src/modelos/entidadeDAO/CidadeDAO.java
+++ src/modelos/entidadeDAO/CidadeDAO.java
@@ -17,30 +17,7 @@ public class CidadeDAO {
         this.connection = connection;
     }
 
-<<<<<<< HEAD
     public List<Cidade> buscarPorEstado(Estado estado) {
-=======
-    public Cidade encontrar(Long codigo) {
-        String comando = "SELECT * FROM cidade WHERE cod_cidade = ?";
-        try (PreparedStatement ps = connection.prepareStatement(comando)) {
-            ps.setLong(1, codigo);
-            ResultSet resultado = ps.executeQuery();
-            if (resultado.next()) {
-                EstadoDAO estadoDAO = new EstadoDAO(connection);
-                return new Cidade (
-                    resultado.getLong("cod_cidade"),
-                    resultado.getString("nome_cidade"),
-                    estadoDAO.encontrar(resultado.getLong("cod_estado"))
-                );
-            }
-        } catch (Exception erro) {
-            System.out.println("Erro: " + erro.getMessage());
-        }
-        return null;
-    } 
-
-    public List<Cidade> buscarTodos() {
->>>>>>> e111689d7a32f85901e8995deac1cd77fae3140e
         List<Cidade> cidades = new ArrayList<>();
         String comando = "SELECT * FROM cidade WHERE cod_estado = ?";
         try (PreparedStatement ps = connection.prepareStatement(comando)) {
